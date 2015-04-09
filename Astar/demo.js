e1aa7983d7f29cd5329cb8bf36f5b25d30109bc2
Michael Thomas <thomas13@uga.edu>
2015-04-09 10:02:30 -0400

    General fixes

--- Astar/demo.js
+++ Astar/demo.js
@@ -205,7 +205,7 @@ GraphSearch.prototype.animatePath = function(path) {
     var addClass = function(path, i) {
         if(i >= path.length) { // Finished showing path, now remove
             console.log(path.length);
-            document.write("Path length" + path.length);
+            document.getElementById("length").innerHTML = path.length;
           return removeClass(path, 0);
         }
         elementFromNode(path[i]).addClass(css.active);
