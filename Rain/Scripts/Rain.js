bb1e2afcbc31d3a48840869e8c40affaee8ece75
Patrick Thomas <thomas.michael95@gmail.com>
2016-12-05 22:30:56 -0500

    dsf

--- Rain/Scripts/Rain.js
+++ Rain/Scripts/Rain.js
@@ -1,24 +1,15 @@
-// **** Variables *****
-// Center point
-var centerX   = 0.0;
-var centerY   = 0.0;
+// center point
+var centerX = 0.0, centerY = 0.0;
 
-// Geometry
-var radius    = 45;
-var rotAngle  = -90;
+var radius = 45, rotAngle = -90;
+var accelX = 0.0, accelY = 0.0;
+var deltaX = 0.0, deltaY = 0.0;
+var springing = 0.0009, damping = 0.98;
 
-// physics
-var accelerationX = 0.0;
-var accelerationY = 0.0;
-var deltaX = 0.0;
-var deltaY = 0.0;
-var springing = 0.0009;
-var damping = 0.98;
+//corner nodes
+var nodes = 5;
 
-// Corner nodes
-var cornerNodes = 5;
-
-// Zero fill arrays
+//zero fill arrays
 var nodeStartX = [];
 var nodeStartY = [];
 var nodeX = [];
@@ -26,87 +17,89 @@ var nodeY = [];
 var angle = [];
 var frequency = [];
 
-// Soft-body dynamics
-var organicConstant = 1.0
+// soft-body dynamics
+var organicConstant = 1.0;
 
-// window size
-const WIDTH = 1920;
-const HEIGHT = 1080;
+function setup() {
+  createCanvas(710, 400);
 
-// ****** Functions *******
+  //center shape in window
+  centerX = width/2;
+  centerY = height/2;
 
-function Setup(){
-  createCanvas(WIDTH, HEIGHT);
+  //initialize arrays to 0
+  for (var i=0; i<nodes; i++){
+    nodeStartX[i] = 0;
+    nodeStartY[i] = 0;
+    nodeY[i] = 0;
+    nodeY[i] = 0;
+    angle[i] = 0;
+  }
 
-   // center shape in window
-   centerX = width/2;
-   centerY = height/2;
+  // iniitalize frequencies for corner nodes
+  for (var i=0; i<nodes; i++){
+    frequency[i] = random(5, 12);
+  }
 
-   for(var i = 0; i < cornerNodes; i++){
-     nodeStartX[i] = 0;
-     nodeStartY[i] = 0;
+  noStroke();
+  frameRate(30);
+}
 
-     nodeY[i] = 0;
-     nodeX[i] = 0;
-
-     angle[i] = 0;
-
-     //initialize frequencies of corner nodes
-     for(var i = 0; i < cornerNodes; i++){
-       frequency[i] = random(5, 12);
-     }
-
-     noStroke();
-     frameRate(60);
-   }
-} // End Setup
-
-function draw(){
-  // fade background
+function draw() {
+  //fade background
   fill(0, 100);
-  rect(0, 0, width, height);
+  rect(0,0,width, height);
   drawShape();
   moveShape();
-}// End drawShape
+}
 
-function drawShape(){
-  // calculate the starting locations
-  for(var i = 0; i < cornerNodes; i++){
-    nodeStartX[i] = centerX + cos(radians(rotAngle)) * radius;
-    nodeStartY[i] = centerY + sin(radians(rotAngle)) * radius;
-
-    rotAngle += 360.0/cornerNodes;
+function drawShape() {
+  //  calculate node  starting locations
+  for (var i=0; i<nodes; i++){
+    nodeStartX[i] = centerX+cos(radians(rotAngle))*radius;
+    nodeStartY[i] = centerY+sin(radians(rotAngle))*radius;
+    rotAngle += 360.0/nodes;
   }
-} // End drawShape
 
-function moveShape(){
-  // move the center point
-  deltaX = mouseX - centerX;
-  deltaY = mouseY - centerY;
+  // draw polygon
+  curveTightness(organicConstant);
+  fill(255);
+  beginShape();
+  for (var i=0; i<nodes; i++){
+    curveVertex(nodeX[i], nodeY[i]);
+  }
+  for (var i=0; i<nodes-1; i++){
+    curveVertex(nodeX[i], nodeY[i]);
+  }
+  endShape(CLOSE);
+}
 
-  // creating the springing effect
+function moveShape() {
+  //move center point
+  deltaX = mouseX-centerX;
+  deltaY = mouseY-centerY;
+
+  // create springing effect
   deltaX *= springing;
   deltaY *= springing;
+  accelX += deltaX;
+  accelY += deltaY;
 
-  accelerationX *= deltaX;
-  accelerationY *= deltaY;
+  // move predator's center
+  centerX += accelX;
+  centerY += accelY;
 
-  // move the center
-  centerX += accelerationX;
-  centerY += accelerationY;
+  // slow down springing
+  accelX *= damping;
+  accelY *= damping;
 
-  // slow the springing
-  accelerationX *= damping;
-  accelerationY *= damping;
+  // change curve tightness
+  organicConstant = 1-((abs(accelX)+abs(accelY))*0.1);
 
-  organicConstant = 1 - ((abs(accelerationX) + abs(accelerationY)) * 0.1);
-
-  // move nodes
-  for(var i = 0; i < cornerNodes; i++){
-    nodeX[i] = nodeStartX[i] + sin(radians(angle[i])) * (accelerationX * 2);
-    nodeY[i] = nodeStartY[i] + sin(radians(angle[i])) * (accelerationY * 2);
+  //move nodes
+  for (var i=0; i<nodes; i++){
+    nodeX[i] = nodeStartX[i]+sin(radians(angle[i]))*(accelX*2);
+    nodeY[i] = nodeStartY[i]+sin(radians(angle[i]))*(accelY*2);
     angle[i] += frequency[i];
-
   }
-
 }
